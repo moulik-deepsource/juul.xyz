ec08efe643b2fd2b95bc880170b80dde48aa97d0
Martin Juul <code@juul.xyz>
2021-01-23 17:11:39 +0100

    Style contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,69 @@
+<template functional>
+  <form
+    action="https://send.pageclip.co/XXb20PtxGWiDxWkp93Lvw20mXhmnIUpd/contact-form"
+    class="pageclip-form flex flex-col w-full justify-center"
+    method="post"
+  >
+
+    <label for="email-input">E-mail</label>
+    <input
+      id="email-input"
+      class="p-2 border-2 border-gray-200 rounded"
+      type="email"
+      name="email"
+      placeholder="E-Mail Address"
+      required
+    />
+
+    <label for="subject-input" class="mt-2">Subject</label>
+    <input
+      id="subject-input"
+      class="p-2 border-2 border-gray-200 rounded"
+      type="text"
+      name="subject"
+      placeholder="Hello Martin"
+      autocomplete="off"
+      required
+    />
+
+    <label for="message-textarea" class="mt-2">Message</label>
+    <textarea
+      id="message-textarea"
+      class="p-2 border-2 border-gray-200 rounded"
+      name="body"
+      minlength="200"
+      autocomplete="off"
+      required
+    ></textarea>
+
+    <button
+      type="submit"
+      class="pageclip-form__submit submit-btn mt-2 hover:bg-blue-50 ease-in-out"
+    >
+      <span>Send</span>
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+}
+</script>
+
+<style>
+.submit-btn {
+    border: double 2px transparent;
+    background-image: linear-gradient(#ffffff, #ffffff),
+    linear-gradient(270deg, #00c5d7, #B95DD7 50%, #FFB367 100%);
+    background-clip: content-box, border-box;
+    background-repeat: no-repeat;
+    background-size: 100% 100%, 100% 200%;
+    background-position: 0 0, 0 100%;
+    background-origin: padding-box, border-box;
+    max-width: 50px;
+    border-radius: 10%;
+    align-self: center;
+    padding: 2px;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,49 +1,28 @@
 <template>
   <div class="contact container flex flex-col justify-center items-center">
-    <h1 class="text-5xl mb-10 font-bold">Find me here.</h1>
+    <h1 class="text-5xl mb-10 font-bold">Contact</h1>
 
-    <div class="bg-black p-5 shadow-2xl">
-      <div class="bg-white p-1">
-        <div class="flex flex-col text-3xl bg-black text-white p-10">
-          <a
-              class="hover:underline ease-in-out"
-              href="https://github.com/martin-juul"
-              target="_blank"
-              rel="noopener nofollow"
-          >github.com/martin-juul</a>
+    <ContactForm />
 
-          <a
-              class="hover:underline ease-in-out"
-              href="https://www.linkedin.com/in/martin-juul"
-              target="_blank"
-              rel="noopener nofollow"
-          >linkedin.com/in/martin-juul</a>
-        </div>
-      </div>
+    <div class="flex mt-10">
+      <a href="https://github.com/martin-juul" target="_blank" rel="nofollow noreferrer">
+        <Github />
+      </a>
     </div>
-
-    <form action="https://send.pageclip.co/XXb20PtxGWiDxWkp93Lvw20mXhmnIUpd/contact-form" class="pageclip-form" method="post">
-
-      <label for="email-input">E-mail</label>
-      <input id="email-input" type="email" name="email" placeholder="johndoe@example.com" required />
-
-      <label for="subject-input">Subject</label>
-      <input id="subject-input" type="text" name="subject" value="Hello Martin" required/>
-
-      <label for="message-textarea">Message</label>
-      <textarea id="message-textarea" name="body" minlength="200" required></textarea>
-
-      <button type="submit" class="pageclip-form__submit">
-        <span>Send</span>
-      </button>
-    </form>
 
   </div>
 </template>
 
 <script>
+import ContactForm from '@/components/ContactForm';
+import { Github } from '@icon-park/vue';
+
 export default {
   name: 'Contact',
+  components: {
+    ContactForm,
+    Github,
+  },
   mounted() {
     document.title = 'Contact | Juul';
     document.querySelector('meta[name="description"]').setAttribute('content', 'Where to find me');
